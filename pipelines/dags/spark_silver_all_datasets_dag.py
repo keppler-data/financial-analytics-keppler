5698ee3923160d5f137fe9d8d77c826b9e24853f
geissler01 <geisleraldana@gmail.com>
2026-06-26 10:27:42 -0500

    fix(dag): revert airflow sdk import to airflow models

--- pipelines/dags/spark_silver_all_datasets_dag.py
+++ pipelines/dags/spark_silver_all_datasets_dag.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
-from airflow.sdk import Variable
+from airflow.models import Variable
 
 # Argumentos por defecto del DAG
 default_args = {
